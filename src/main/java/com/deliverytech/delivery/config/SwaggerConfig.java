7bd101cfd2c0a97719fd5193f7685521dd60f976
Luan Carlos <luancarloscar2@gmail.com>
2025-11-13 20:47:48 -0300

    add porn.xml

---
+++ src/main/java/com/deliverytech/delivery/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.deliverytech.delivery.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Delivery API")
+                        .description("API para sistema de delivery de comida")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("DeliveryTech Team")
+                                .email("contato@deliverytech.com")));
+    }
+}
